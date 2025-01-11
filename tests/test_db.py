c9a5f0dc3f00460c18fa29cfa0e273cadba384e9
Pedro Miranda <113561981+pLogicador@users.noreply.github.com>
2025-01-11 00:02:23 -0300

    feat: add updated_at field to User model and implement database migration

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,18 +1,29 @@
+from dataclasses import asdict
+
 from sqlalchemy import select
 
 from fastpy_todo.models import User
 
 
-def test_create_user(session):
-    user = User(
-        username='testusername',
-        email='test@test.com',
-        password='my_password',
-    )
+def test_create_user(session, mock_db_time):
+    with mock_db_time(model=User) as time:
+        new_user = User(
+            username='testusername',
+            email='test@test.com',
+            password='my_password',
+        )
+        session.add(new_user)
+        session.commit()
 
-    session.add(user)
-    session.commit()
+        user = session.scalar(
+            select(User).where(User.username == 'testusername')
+        )
 
-    result = session.scalar(select(User).where(User.email == 'test@test.com'))
-
-    assert result.id == 1
+        assert asdict(user) == {
+            'id': 1,
+            'username': 'testusername',
+            'password': 'my_password',
+            'email': 'test@test.com',
+            'created_at': time,
+            'updated_at': time,
+        }
